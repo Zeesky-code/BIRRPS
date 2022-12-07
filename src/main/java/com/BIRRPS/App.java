48f70182382e3bc26adc77e2c564bf12556d05d2
Zainab Lawal <zainablaw1012@gmail.com>
2022-12-07 14:33:25 +0300

    Created a system class that would hold all the potential system methods that would be called. For simplification

--- src/main/java/com/BIRRPS/App.java
+++ src/main/java/com/BIRRPS/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        BIRRPSSystem BikeSystem = new BIRRPSSystem();
+        BikeSystem.welcome();
+        User newUser = new User("John", "Doe", "5340407341");
+
         System.out.println( "Hello World!" );
     }
 }
